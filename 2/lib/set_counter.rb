679be2e8325655f0b29021a4450193e535fe74ae
Love Ottosson <loveo@kth.se>
2016-11-15 10:32:11 +0100

    Add threads to SetCounter

--- 2/lib/set_counter.rb
+++ 2/lib/set_counter.rb
@@ -7,15 +7,12 @@ class SetCounter
   def initialize(baskets)
     @baskets    = baskets
     @threshold  = baskets.rows.length * FREQUENCY_THRESHOLD
-    @mutex      = Mutex.new
-    @jobs       = []
+    @job_queue  = Queue.new
   end
 
   # Counts an array for item sets by checking each bucket
   def count_item_sets(item_sets)
-    @baskets.rows.each do |basket|
-      SetCounter.count_all_item_sets_in_basket(basket, item_sets)
-    end
+    threaded_count(item_sets)
   end
 
   # Returns an array of item sets with high enough support
@@ -27,20 +24,40 @@ class SetCounter
 
   private
 
-  def threaded_count
-    @jobs = (0 .. @baskets.rows.length)
+  # Creates threads that counts item sets in baskets
+  # and waits for them to finish
+  def threaded_count(item_sets)
+    create_job_queue
 
-    workers = (0 .. THREAD_SIZE).each.map do 
-      Thread.new { threaded_basket_count() }
-    end
-
-    workers.each do |thread|
+    create_workers(item_sets).each do |thread|
       thread.join
     end
   end
 
-  def threaded_basket_count
+  # Puts each basket on the job queue
+  def create_job_queue
+    (0 .. @baskets.rows.length - 1).each do |row_index|
+      @job_queue << row_index
+    end
+  end
 
+  # Creates worker threads and tells them to count baskets
+  def create_workers(item_sets)
+    (0 .. THREAD_SIZE - 1).each.map do 
+      Thread.new { threaded_basket_count(item_sets) }
+    end
+  end
+
+  # Thread method that takes baskets from a job queue and counts them
+  def threaded_basket_count(item_sets)
+    while not @job_queue.empty?
+      row_index = @job_queue.pop
+
+      SetCounter.count_all_item_sets_in_basket(
+        @baskets.rows[row_index],
+        item_sets
+        )
+    end
   end
 
   # Counts the support of all item sets in a given basket
--- 2/lib/test.rb
+++ 2/lib/test.rb
@@ -1,5 +1,7 @@
 require_relative 'frequent_items'
 
+time = Time.now
+
 baskets    = Baskets.new('data/baskets.dat')
 singletons = ItemCounter.new(baskets).create_singletons
 doubletons = SetCreator.create_doubletons(singletons)
@@ -8,6 +10,8 @@ set_counter = SetCounter.new(baskets)
 set_counter.count_item_sets(doubletons)
 
 doubletons = set_counter.supported_sets(doubletons)
+
+puts "Time elapsed: #{Time.now - time}"
 
 byebug
 
