47ce0dd58c61116aadc34a7df8781a105754bcb1
Rickard Zwahlen <rickardz@kth.se>
2016-11-23 15:34:33 +0100

    Implemented one pass of hyperball

--- 3/lib/hyperball.rb
+++ 3/lib/hyperball.rb
@@ -1,6 +1,19 @@
 
 class Hyperball
-
-
-
+	
+	def initialize(counters)
+		@counters = counters
+	end
+	
+	
+	#Runs one pass of hyperball
+	def hyperball(edges)
+		edges.each do |edge|
+			update_node = @counters(edge[1])
+			neighbor_node = @counters(edge[0])
+			update_node.merge(neighbor_node)
+		end
+			@counters.apply_changes
+	end
+	
 end
