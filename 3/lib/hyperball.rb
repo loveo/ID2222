08525ad860779c687f12490ff3b9fe55da723d64
Rickard Zwahlen <rickardz@kth.se>
2016-11-23 15:54:13 +0100

    Hyperball runs until no more changes

--- 3/lib/hyperball.rb
+++ 3/lib/hyperball.rb
@@ -6,14 +6,21 @@ class Hyperball
 	end
 	
 	
-	#Runs one pass of hyperball
-	def hyperball(edges)
+	#Runs one pass of hyperball	
+	def hyperball_once(edges)
 		edges.each do |edge|
 			update_node = @counters(edge[1])
 			neighbor_node = @counters(edge[0])
 			update_node.merge(neighbor_node)
 		end
-			@counters.apply_changes
+		@counters.apply_changes
+		edges
+	end
+	
+	def hyperball(edges)
+		while @counters.any_changes? do
+			hyperball_once(edges)
+		end
 	end
 	
 end
