c8f748521ff08ba27b67ffbab1d230576ca0055b
Rickard Zwahlen <rickardz@kth.se>
2016-12-16 16:15:11 +0100

    Attempt at plotting Fiedler vector, it's inverted somehow

--- 4/graph_spectra.m
+++ 4/graph_spectra.m
@@ -10,37 +10,31 @@ matrix_size = max(max(col1,col2));
 As = sparse(col1, col2, 1, matrix_size, matrix_size);
 A = full(As);
 G = graph(A,'OmitSelfLoops');
+figure(1);
 H = plot(G,'-or');
+title('Graph');
 D_inv = diag(1./sqrt(sum(A, 2)));
 L = D_inv * A * D_inv;
 
 [X,~] = eigs(L, k);
 Y = normr(X);
+[E,D] = eigs(L,2,'SA');
+
 
 c = kmeans(Y, k);
-
-
-
 K = zeros(k,matrix_size);
-%for i=1:k
- %   K = [K, {0}];
-    
-%end
-%K = reshape(K,k,0);
+
 
 
 
 for n = 1:length(c)
     clusterIndex = c(n);
-    %K(clusterIndex) = [K(clusterIndex) n];
     for m = 1:241
         if K(clusterIndex, m) == 0
             K(clusterIndex,m) = n;
             break;
         end
     end
-    
-    %[K(clusterIndex) n]; 
 end
 J = K(1,:);
 J = J(J~=0);
@@ -58,7 +52,10 @@ J = K(4,:);
 J = J(J~=0);
 highlight(H,J, 'NodeColor', 'c');
 
+Sorted = sort(E(:,2));
 
+figure(2);
+plot(transpose(Sorted));
+title('Fiedler');
 
-%A(A~=0)
 
