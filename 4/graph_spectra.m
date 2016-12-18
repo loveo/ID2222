8c3d7d83387572d41e6a8f624999c759d9534f0d
Love Ottosson <loveo@kth.se>
2016-12-18 15:35:15 +0100

    Use loop for node coloring. Add Fiedler plots

--- 4/graph_spectra.m
+++ 4/graph_spectra.m
@@ -1,4 +1,4 @@
-k = 5;
+k = 4;
 
 E = csvread('./data/example1.dat');
 
@@ -9,25 +9,28 @@ matrix_size = max(max(col1,col2));
 
 As = sparse(col1, col2, 1, matrix_size, matrix_size);
 A = full(As);
-G = graph(A,'OmitSelfLoops');
-figure(1);
-H = plot(G,'-or');
-title('Graph');
+
 D_inv = diag(1./sqrt(sum(A, 2)));
 L = D_inv * A * D_inv;
 
-[X,Xv] = eigs(L);
+[X,~] = eigs(L, k);
 Y = normr(X);
 
-
 c = kmeans(Y, k);
+
+
+% Plotting
+
+G = graph(A,'OmitSelfLoops');
+H = plot(G,':ok');
+
+% Place nodes in clusters
+
 K = zeros(k,matrix_size);
-
-
-
 
 for n = 1:length(c)
     clusterIndex = c(n);
+
     for m = 1:241
         if K(clusterIndex, m) == 0
             K(clusterIndex,m) = n;
@@ -36,27 +39,17 @@ for n = 1:length(c)
     end
 end
 
-J = K(1,:);
-J = J(J~=0);
-highlight(H,J, 'NodeColor', 'g');
+% Highlight nodes
+colors = ['g', 'b', 'y', 'c', 'm', 'k'];
 
-J = K(2,:);
-J = J(J~=0);
-highlight(H,J, 'NodeColor', 'y');
+for n = 1:k
+   J = K(n,:);
+   J = J(J~=0);
+   highlight(H, J, 'NodeColor', colors(n));
+end
 
-J = K(3,:);
-J = J(J~=0);
-highlight(H,J, 'NodeColor', 'b');
-
-J = K(4,:);
-J = J(J~=0);
-highlight(H,J, 'NodeColor', 'c');
-
-%Eigenvalue 5 is the second smallest.
-Sorted = sort(X(:,5));
+% Fiedler plot
 
 figure(2);
-plot(transpose(Sorted));
+plot(X);
 title('Fiedler');
-
-
